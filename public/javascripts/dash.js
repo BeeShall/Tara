c8c42668bde489099384df631855866791395e90
Bishal Regmi <beeshall@Bishals-MBP.dkn.io>
2017-04-02 07:40:15 -0700

    Made the basic frontend

--- public/javascripts/dash.js
+++ public/javascripts/dash.js
@@ -1,13 +1,39 @@
+var allFriends = [{label: "sujil", id:2093758937},{label:"Bishal", id:0823649635947}];
+var selectedItem;
 $(document).ready(function() {
+    initializeDisplays();
+
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myModal').focus()
+        })
+
     $('#nextID').on('click', function () {
         FB.api('/me', function(response) {
             console.log(response);
             var userID = response.id;
 
             getAllFriends('me');
-
+            $('.started').hide();
+            $('#profile').fadeIn();
         });
+
+        
     });
+
+    $('#searchID').autocomplete({
+        source: allFriends,
+        select:function (event, ui) {
+                for (var i in allFriends) {
+                    if (allFriends[i].label == ui.item.label){
+                        selectedItem = allFriends[i];
+
+                        // Call it!
+                        //alert(selectedItem.label + selectedItem.id);
+                        
+                    }
+                }
+            }
+    })
 
     //  Handles on click if friend is clicked.
     $('#friendList').on('click', 'li', function () {
@@ -38,7 +64,49 @@ $(document).ready(function() {
             }
         );
     });
+
+    $('#goButton').on('click', function () {
+        if (selectedItem) {
+            values = findValues(selectedItem.id);
+
+            $.get('/analyze', {
+                    data: values
+                },
+                function (data, success) {
+                    if (success) {
+                        // start the modal here.
+                        $('#answer').append("<p>" + data +"</p>")
+                        //return data;
+                    }
+                });
+        }
+    })
+
+
 });
+
+function findValues(id) {
+    /* make the API call */
+        FB.api("/"+id+"/feed",
+            function (response) {
+            if (response && !response.error) {
+                /* handle the result */
+                console.log("Result:")
+                console.log(response);
+
+                var allPosts = response.data;
+                var toSend = [];
+                for (var i in allPosts) {
+                    toSend.push(allPosts[i].message);
+                }
+
+                return toSend;
+
+                
+            }
+            }
+        );
+}
 
 function getAllFriends(id) {
     // Returns all the friends.
@@ -47,10 +115,16 @@ function getAllFriends(id) {
 
         for (var i = 0; i < friends.length; i++){
             console.log(friends[i])
-            $('#friendList').append("<li>"+friends[i].name+"</li>");
-
-            $('#friendList').children().last().data("id", friends[i].id);
+            //$('#friendList').append("<li>"+friends[i].name+"</li>");
+            allFriends.push(friends[i]);
+            //$('#friendList').children().last().data("id", friends[i].id);
         }
     });
 };
 
+
+function initializeDisplays() {
+    $('#facebookLogin').hide();
+    $('.started').show();
+    $('#profile').hide();
+}
